8cd2f3e770625b7c3eb417ed5ba4a512eab6bccb
Intersteller-Pi <cole.hayes.03@gmail.com>
2020-01-22 17:11:30 -0600

    v1.2

--- pidtest/src/main/java/frc/robot/Robot.java
+++ pidtest/src/main/java/frc/robot/Robot.java
@@ -7,7 +7,14 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,17 +28,69 @@ public class Robot extends TimedRobot {
    * This function is run when the robot is first started up and should be used
    * for any initialization code.
    */
-  @Override
-  public void robotInit() {
-  }
+
+  private TalonSRX rF = new TalonSRX(1);
+  private TalonSRX rR = new TalonSRX(2);
+  private TalonSRX lF = new TalonSRX(3);
+  private TalonSRX lR = new TalonSRX(4);
+
+  private Joystick joy1 = new Joystick(0);
+  private Encoder encoder = new Encoder(0, 1, true);
+  private final double kDriveTick2Feet = 1.0/360*Math.PI*4/12;
+
+  final double kP = 0.5;
+  final double kI = 0.0;
+  final double kD = 0.0;
+  final double iLimit = 1;
+
+  double setpoint = 0;
+  double errorSum = 0;
+  double lastTimestamp = 0;
+  double lastError = 0;
 
   @Override
+
   public void autonomousInit() {
+
+    encoder.reset();
+    errorSum = 0;
+    lastError = 0;
+    lastTimestamp = Timer.getFPGATimestamp();
+
   }
 
   @Override
+
   public void autonomousPeriodic() {
+
+    if (joy1.getRawButton(1)) {
+      setpoint = 10;
+    } else if (joy1.getRawButton(2)) {
+      setpoint = 0;
+    }
+
+    double sensorPosition = encoder.get() * kDriveTick2Feet;
+    double error = setpoint - sensorPosition;
+    double dt = Timer.getFPGATimestamp() - lastTimestamp;
+
+    if (Math.abs(error) < iLimit) {
+      errorSum += error * dt;
+    }
+
+
+    double errorRate = (error - lastError) / dt;
+    double outputSpeed = kP * error + kI * errorSum + kD * errorRate;
+
+    lF.set(ControlMode.PercentOutput ,outputSpeed);
+    lR.set(ControlMode.PercentOutput ,outputSpeed);
+    rF.set(ControlMode.PercentOutput ,-outputSpeed);
+    rR.set(ControlMode.PercentOutput ,-outputSpeed);
+
+    lastTimestamp = Timer.getFPGATimestamp();
+    lastError = error;
+
   }
+
 
   @Override
   public void teleopInit() {
