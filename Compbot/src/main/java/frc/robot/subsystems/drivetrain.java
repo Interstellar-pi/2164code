ea843e2e1d932c0124298af47336c94ea9338e33
Intersteller-Pi <cole.hayes.03@gmail.com>
2020-01-25 16:35:45 -0600

    Added Drivetrain (not finished)

---
+++ Compbot/src/main/java/frc/robot/subsystems/drivetrain.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class drivetrain extends SubsystemBase {
+  /**
+   * Creates a new drivetrain.
+   */
+  public drivetrain() {
+    SpeedControllerGroup right = new SpeedControllerGroup(new WPI_TalonSRX(Constants.DriveConstants.rF),new WPI_VictorSPX(Constants.DriveConstants.rR));
+    SpeedControllerGroup left = new SpeedControllerGroup(new WPI_TalonSRX(Constants.DriveConstants.lF),new WPI_VictorSPX(Constants.DriveConstants.lR));
+
+    DifferentialDrive drive = new DifferentialDrive(left, right);
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
