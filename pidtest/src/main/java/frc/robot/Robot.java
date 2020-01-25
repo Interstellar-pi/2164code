e54c3441f3882bc838f97d32809be18469576c6b
Intersteller-Pi <cole.hayes.03@gmail.com>
2020-01-25 10:28:10 -0600

    P complete

--- pidtest/src/main/java/frc/robot/Robot.java
+++ pidtest/src/main/java/frc/robot/Robot.java
@@ -9,12 +9,15 @@ package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -29,21 +32,26 @@ public class Robot extends TimedRobot {
    * for any initialization code.
    */
 
-  private TalonSRX rF = new TalonSRX(1);
-  private TalonSRX rR = new TalonSRX(2);
-  private TalonSRX lF = new TalonSRX(3);
-  private TalonSRX lR = new TalonSRX(4);
+  private final WPI_TalonSRX rF = new WPI_TalonSRX(1);
+  private final WPI_TalonSRX rR = new WPI_TalonSRX(2);
+  private final WPI_TalonSRX lF = new WPI_TalonSRX(3);
+  private final WPI_TalonSRX lR = new WPI_TalonSRX(4);
+
+  private final SpeedControllerGroup right = new SpeedControllerGroup(rF,rR);
+  private final SpeedControllerGroup left = new SpeedControllerGroup(lF,lR);
+  private DifferentialDrive drive = new DifferentialDrive(left, right);
+
 
   private Joystick joy1 = new Joystick(0);
   private Encoder encoder = new Encoder(0, 1, true);
   private final double kDriveTick2Feet = 1.0/360*Math.PI*4/12;
 
-  final double kP = 0.5;
+  final double kP = 0.15;
   final double kI = 0.0;
   final double kD = 0.0;
   final double iLimit = 1;
 
-  double setpoint = 0;
+  double setpoint = 10;
   double errorSum = 0;
   double lastTimestamp = 0;
   double lastError = 0;
@@ -53,7 +61,9 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
 
     encoder.reset();
-    errorSum = 0;
+    //lF.setInverted(true);
+    //lR.setInverted(true);
+    errorSum = 0; 
     lastError = 0;
     lastTimestamp = Timer.getFPGATimestamp();
 
@@ -81,10 +91,7 @@ public class Robot extends TimedRobot {
     double errorRate = (error - lastError) / dt;
     double outputSpeed = kP * error + kI * errorSum + kD * errorRate;
 
-    lF.set(ControlMode.PercentOutput ,outputSpeed);
-    lR.set(ControlMode.PercentOutput ,outputSpeed);
-    rF.set(ControlMode.PercentOutput ,-outputSpeed);
-    rR.set(ControlMode.PercentOutput ,-outputSpeed);
+    drive.arcadeDrive(-outputSpeed, 0);
 
     lastTimestamp = Timer.getFPGATimestamp();
     lastError = error;
