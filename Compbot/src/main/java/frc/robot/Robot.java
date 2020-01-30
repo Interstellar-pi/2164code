ea4a5aaaec0e9d327ec74e9b8141adb74bc22ca3
Intersteller-Pi <cole.hayes.03@gmail.com>
2020-01-30 12:18:16 -0600

    Adding Color Sensor Stuff

--- Compbot/src/main/java/frc/robot/Robot.java
+++ Compbot/src/main/java/frc/robot/Robot.java
@@ -9,11 +9,23 @@ package frc.robot;
 
 import java.util.Map;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatchResult;
+import com.revrobotics.ColorMatch;
+
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -24,8 +36,23 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+ ShuffleboardTab autonTab = Shuffleboard.getTab("Autonomous Selection");
+ ShuffleboardTab mainTab = Shuffleboard.getTab("Main Tab");
 
+  private RobotContainer m_robotContainer;
+ 
+  public NetworkTableEntry n_StartLeft;
+  public NetworkTableEntry n_StartCenter;
+  public NetworkTableEntry n_StartRight;
+
+  I2C.Port i2cPort;
+  ColorSensorV3 m_colorSensor;
+  ColorMatch m_colorMatcher;
+  Color kBlueTarget;
+  Color kGreenTarget;
+  Color kRedTarget;
+  Color kYellowTarget;
+  boolean ColorSensorConfidence;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -35,67 +62,91 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+/*
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);
+*/
 
-    Shuffleboard.getTab("Auton Selection")
+      autonTab
         .add("Starting Left", false)
-          .withWidget(BuiltInWidgets.kToggleSwitch)
-          .withSize(1, 1)
-          .withPosition(1, 1);
+        .withWidget(BuiltInWidgets.kToggleSwitch)
+        .withSize(1, 1)
+        .withPosition(1, 1)
+        .getEntry();
 
-    Shuffleboard.getTab("Auton Selection")
+        autonTab
       .add("Starting Center", false)
-        .withWidget(BuiltInWidgets.kToggleSwitch)
-        .withSize(1, 1)
-        .withPosition(2, 1);
-    
-    Shuffleboard.getTab("Auton Selection")
+      .withWidget(BuiltInWidgets.kToggleSwitch)
+      .withSize(1, 1)
+      .withPosition(2, 1)
+      .getEntry();
+
+      autonTab
       .add("Starting Right", false)
-        .withWidget(BuiltInWidgets.kToggleSwitch)
-        .withSize(1, 1)
-        .withPosition(3, 1);
+      .withWidget(BuiltInWidgets.kToggleSwitch)
+      .withSize(1, 1)
+      .withPosition(3, 1)
+      .getEntry();
     
-    Shuffleboard.getTab("Auton Selection")
+      autonTab
       .add("Other", false)
-        .withWidget(BuiltInWidgets.kToggleSwitch)
-        .withSize(1, 1)
-        .withPosition(4, 1);
+      .withWidget(BuiltInWidgets.kToggleSwitch)
+      .withSize(1, 1)
+      .withPosition(4, 1)
+      .getEntry();
 
-    Shuffleboard.getTab("Auton Selection")
+      autonTab
       .add("Bail Left", false)
-        .withWidget(BuiltInWidgets.kToggleSwitch)
-        .withSize(1, 1)
-        .withPosition(1, 3);
+      .withWidget(BuiltInWidgets.kToggleSwitch)
+      .withSize(1, 1)
+      .withPosition(1, 2)
+      .getEntry();
 
-    Shuffleboard.getTab("Auton Selection")
+      autonTab
       .add("Bail Center", false)
-        .withWidget(BuiltInWidgets.kToggleSwitch)
-        .withSize(1, 1)
-        .withPosition(2, 3);
+      .withWidget(BuiltInWidgets.kToggleSwitch)
+      .withSize(1, 1)
+      .withPosition(2, 2)
+      .getEntry();
 
-    Shuffleboard.getTab("Auton Selection")
+      autonTab
       .add("Bail Right", false)
-        .withWidget(BuiltInWidgets.kToggleSwitch)
-        .withSize(1, 1)
-        .withPosition(3, 3);
+      .withWidget(BuiltInWidgets.kToggleSwitch)
+      .withSize(1, 1)
+      .withPosition(3, 2)
+      .getEntry();
 
-    Shuffleboard.getTab("Auton Selection")
+      autonTab
       .add("No Bail", false)
-        .withWidget(BuiltInWidgets.kToggleSwitch)
-        .withSize(1, 1)
-        .withPosition(4, 3);
+      .withWidget(BuiltInWidgets.kToggleSwitch)
+      .withSize(1, 1)
+      .withPosition(4, 2)
+      .getEntry();
     
-    Shuffleboard.getTab("Auton Selection")
+      autonTab
       .add("Delay Start", 0)
-        .withWidget(BuiltInWidgets.kNumberSlider)
-        .withProperties(Map.of("Min", 0, "Max", 13))
-        .withSize(2, 1)
-        .withPosition(1, 4);
-    
+      .withWidget(BuiltInWidgets.kNumberSlider)
+      .withProperties(Map.of("Min", 0, "Max", 13))
+      .withSize(2, 1)
+      .withPosition(1, 3)
+      .getEntry();
 
-      
-                
-      
-  
+    
+      mainTab
+      .add("WOF Target Color", "Unrecived")
+        .withWidget(BuiltInWidgets.kTextView)
+        .withSize(1, 1)
+        .withPosition(1, 4)
+        .getEntry();
+    
+      mainTab
+      .add("Color Detected", "N/A")
+        .withWidget(BuiltInWidgets.kTextView)
+        .withSize(1, 1)
+        .withPosition(2, 4)
+        .getEntry();
 
   }
 
@@ -112,6 +163,66 @@ public class Robot extends TimedRobot {
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
+   /* if(RobotBase.isReal()){
+
+    i2cPort = I2C.Port.kOnboard;
+    m_colorSensor = new ColorSensorV3(i2cPort);
+    m_colorMatcher = new ColorMatch();
+    kBlueTarget = ColorMatch.makeColor(0.16, 0.39, 0.44);
+    kGreenTarget = ColorMatch.makeColor(0.19, 0.55, 0.25);
+    kRedTarget = ColorMatch.makeColor(0.4, 0.4, 0.17);
+    kYellowTarget = ColorMatch.makeColor(.307, 0.547, 0.144);
+    ColorSensorConfidence = false;
+
+
+    Color detectedColor = m_colorSensor.getColor();
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+    String colorString = "Unknown";
+
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
+    if (match.color == kBlueTarget && match.confidence >= .98) ColorSensorConfidence = true;
+    else if (match.color == kBlueTarget && match.confidence <= .979) ColorSensorConfidence = false;
+  }
+    
+    /*  SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
+    SmartDashboard.putBoolean("Color Accurate", ColorSensorConfidence);
+    */
+    String gameData;
+    gameData = DriverStation.getInstance().getGameSpecificMessage();
+    if(gameData.length() > 0)
+    {
+      switch (gameData.charAt(0))
+      {
+        case 'B' :
+          break;
+        case 'G' :
+          break;
+        case 'R' :
+          break;
+        case 'Y' :
+          break;
+        default :
+          break;
+      }
+    } else {
+    }
+
+
+
     CommandScheduler.getInstance().run();
   }
 
