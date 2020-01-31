39d876e5b7a298bee9e712aa44badf019d8eb2bc
Intersteller-Pi <cole.hayes.03@gmail.com>
2020-01-31 15:29:41 -0600

    Fixed some errors

--- Compbot/src/main/java/frc/robot/Robot.java
+++ Compbot/src/main/java/frc/robot/Robot.java
@@ -15,24 +15,16 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.util.Color;
 import com.revrobotics.ColorSensorV3;
-import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 
 
-
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
@@ -44,6 +36,14 @@ public class Robot extends TimedRobot {
   public NetworkTableEntry n_StartLeft;
   public NetworkTableEntry n_StartCenter;
   public NetworkTableEntry n_StartRight;
+  public NetworkTableEntry n_Other;
+  public NetworkTableEntry n_BailLeft;
+  public NetworkTableEntry n_BailCenter;
+  public NetworkTableEntry n_BailRight;
+  public NetworkTableEntry n_NoBail;
+  public NetworkTableEntry n_StartDelay;
+  public NetworkTableEntry n_ColorNeeded;
+  public NetworkTableEntry n_ColorDetected;
 
   I2C.Port i2cPort;
   ColorSensorV3 m_colorSensor;
@@ -53,14 +53,11 @@ public class Robot extends TimedRobot {
   Color kRedTarget;
   Color kYellowTarget;
   boolean ColorSensorConfidence;
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  String colorString = "Unknown";
+
+
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
 /*
     m_colorMatcher.addColorMatch(kBlueTarget);
@@ -69,63 +66,63 @@ public class Robot extends TimedRobot {
     m_colorMatcher.addColorMatch(kYellowTarget);
 */
 
-      autonTab
-        .add("Starting Left", false)
-        .withWidget(BuiltInWidgets.kToggleSwitch)
-        .withSize(1, 1)
-        .withPosition(1, 1)
-        .getEntry();
+      n_StartLeft = autonTab
+      .add("Starting Left", false)
+      .withWidget(BuiltInWidgets.kToggleSwitch)
+      .withSize(1, 1)
+      .withPosition(1, 1)
+      .getEntry();
 
-        autonTab
+      n_StartCenter = autonTab
       .add("Starting Center", false)
       .withWidget(BuiltInWidgets.kToggleSwitch)
       .withSize(1, 1)
       .withPosition(2, 1)
       .getEntry();
 
-      autonTab
+      n_StartRight = autonTab
       .add("Starting Right", false)
       .withWidget(BuiltInWidgets.kToggleSwitch)
       .withSize(1, 1)
       .withPosition(3, 1)
       .getEntry();
     
-      autonTab
+      n_Other = autonTab
       .add("Other", false)
       .withWidget(BuiltInWidgets.kToggleSwitch)
       .withSize(1, 1)
       .withPosition(4, 1)
       .getEntry();
 
-      autonTab
+      n_BailLeft = autonTab
       .add("Bail Left", false)
       .withWidget(BuiltInWidgets.kToggleSwitch)
       .withSize(1, 1)
       .withPosition(1, 2)
       .getEntry();
 
-      autonTab
+      n_BailCenter = autonTab
       .add("Bail Center", false)
       .withWidget(BuiltInWidgets.kToggleSwitch)
       .withSize(1, 1)
       .withPosition(2, 2)
       .getEntry();
 
-      autonTab
+      n_BailRight = autonTab
       .add("Bail Right", false)
       .withWidget(BuiltInWidgets.kToggleSwitch)
       .withSize(1, 1)
       .withPosition(3, 2)
       .getEntry();
 
-      autonTab
+      n_NoBail = autonTab
       .add("No Bail", false)
       .withWidget(BuiltInWidgets.kToggleSwitch)
       .withSize(1, 1)
       .withPosition(4, 2)
       .getEntry();
     
-      autonTab
+      n_StartDelay = autonTab
       .add("Delay Start", 0)
       .withWidget(BuiltInWidgets.kNumberSlider)
       .withProperties(Map.of("Min", 0, "Max", 13))
@@ -134,14 +131,14 @@ public class Robot extends TimedRobot {
       .getEntry();
 
     
-      mainTab
-      .add("WOF Target Color", "Unrecived")
+     n_ColorNeeded = mainTab
+      .add("Target Color", "Unrecived")
         .withWidget(BuiltInWidgets.kTextView)
         .withSize(1, 1)
         .withPosition(1, 4)
         .getEntry();
     
-      mainTab
+     n_ColorDetected = mainTab
       .add("Color Detected", "N/A")
         .withWidget(BuiltInWidgets.kTextView)
         .withSize(1, 1)
@@ -150,20 +147,12 @@ public class Robot extends TimedRobot {
 
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
+
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-   /* if(RobotBase.isReal()){
+   
+    //   VV VV Ignore this VV VV
+    if(RobotBase.isReal()){ 
 
     i2cPort = I2C.Port.kOnboard;
     m_colorSensor = new ColorSensorV3(i2cPort);
@@ -177,7 +166,7 @@ public class Robot extends TimedRobot {
 
     Color detectedColor = m_colorSensor.getColor();
     ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
-    String colorString = "Unknown";
+    
 
     if (match.color == kBlueTarget) {
       colorString = "Blue";
@@ -194,13 +183,6 @@ public class Robot extends TimedRobot {
     else if (match.color == kBlueTarget && match.confidence <= .979) ColorSensorConfidence = false;
   }
     
-    /*  SmartDashboard.putNumber("Red", detectedColor.red);
-    SmartDashboard.putNumber("Green", detectedColor.green);
-    SmartDashboard.putNumber("Blue", detectedColor.blue);
-    SmartDashboard.putNumber("Confidence", match.confidence);
-    SmartDashboard.putString("Detected Color", colorString);
-    SmartDashboard.putBoolean("Color Accurate", ColorSensorConfidence);
-    */
     String gameData;
     gameData = DriverStation.getInstance().getGameSpecificMessage();
     if(gameData.length() > 0)
@@ -221,14 +203,13 @@ public class Robot extends TimedRobot {
     } else {
     }
 
+      n_ColorNeeded.setString(gameData);
+      n_ColorDetected.setString(colorString);
 
 
     CommandScheduler.getInstance().run();
   }
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   */
   @Override
   public void disabledInit() {
   }
@@ -237,53 +218,39 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {
   }
 
-  /**
-   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-   */
+
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
-    // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
       m_autonomousCommand.schedule();
     }
   }
 
-  /**
-   * This function is called periodically during autonomous.
-   */
+
+  
   @Override
   public void autonomousPeriodic() {
   }
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
   }
 
-  /**
-   * This function is called periodically during operator control.
-   */
+
   @Override
   public void teleopPeriodic() {
   }
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /**
-   * This function is called periodically during test mode.
-   */
   @Override
   public void testPeriodic() {
   }
