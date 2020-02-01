484bfd5268a541d6e4f8bd876bb8fc7436fbfb6c
Phantom <30608203+Phantom2x5@users.noreply.github.com>
2020-02-01 16:21:23 -0600

    Rewrite v0.7

--- Alpha/MainBot/src/main/java/frc/robot/RobotContainer.java
+++ Alpha/MainBot/src/main/java/frc/robot/RobotContainer.java
@@ -18,16 +18,14 @@ import frc.robot.commands.CollectorShoot;
 import frc.robot.commands.Drive;
 import frc.robot.commands.LiftExtend;
 import frc.robot.commands.LiftWinch;
-import frc.robot.commands.WoFArm;
 import frc.robot.commands.WoFArmToggle;
+import frc.robot.subsystems.WoFArm;
 import frc.robot.commands.WoFSpin;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Collector;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Lift;
 import frc.robot.subsystems.LiftExtender;
 import frc.robot.subsystems.LiftWincher;
-import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.WoF;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -64,6 +62,7 @@ public class RobotContainer {
   private final LiftWincher rc_LiftWincher = new LiftWincher();
   private final LiftWinch rc_LiftWinch = new LiftWinch(rc_LiftWincher);
   private final WoFArm rc_WoFArm = new WoFArm();
+  private final WoFArmToggle rc_WoFArmToggle = new WoFArmToggle(rc_WoFArm);
   private final WoF rc_WoF = new WoF();
   private final WoFSpin rc_WoFSpin = new WoFSpin(rc_WoF);
 
