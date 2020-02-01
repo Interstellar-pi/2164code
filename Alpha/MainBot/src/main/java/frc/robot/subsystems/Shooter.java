2088d662158edb26e268eebbe06b353cc3edaba0
Phantom <30608203+Phantom2x5@users.noreply.github.com>
2020-02-01 15:46:20 -0600

    Rewrite v0.4

--- Alpha/MainBot/src/main/java/frc/robot/subsystems/Shooter.java
+++ Alpha/MainBot/src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,10 +10,10 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
-public class Shooter extends CommandBase {
+public class Shooter extends SubsystemBase {
 
   private final WPI_TalonSRX IntakeMotor = new WPI_TalonSRX(ArmConstants.CAN_Intake);
 
@@ -29,28 +29,15 @@ public class Shooter extends CommandBase {
     IntakeMotor.set(ControlMode.PercentOutput, 0);
   }
 
+  /**
+   * Creates a new Shooter.
+   */
   public Shooter() {
-    // Use addRequirements() here to declare subsystem dependencies.
+
   }
 
-  // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 }
