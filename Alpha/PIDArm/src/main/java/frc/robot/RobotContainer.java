fc9106b8910c583f3cc0257161ade8c8c07f846e
Phantom <30608203+Phantom2x5@users.noreply.github.com>
2020-02-15 17:19:59 -0600

    Changed encoder type and removed from sparkmax direct connect

--- Alpha/PIDArm/src/main/java/frc/robot/RobotContainer.java
+++ Alpha/PIDArm/src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.PIDArm;
 
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -18,13 +19,9 @@ import edu.wpi.first.wpilibj2.command.Command;
  * (including subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
 
+  private final PIDArm rc_PIDArm = new PIDArm();
 
-
-  /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
-   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
--- Alpha/PIDArm/src/main/java/frc/robot/subsystems/PIDArm.java
+++ Alpha/PIDArm/src/main/java/frc/robot/subsystems/PIDArm.java
@@ -7,16 +7,29 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import frc.robot.Constants.ArmConstants;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class PIDArm extends PIDSubsystem {
   
-  private final CANSparkMax SpinMotor = new CANSparkMax(ArmConstants.CAN_Pitch, MotorType.kBrushed);
+  private final CANSparkMax PitchMotor = new CANSparkMax(ArmConstants.CAN_Pitch, MotorType.kBrushed);
+
+  //private final CANEncoder ArmEncoder = new CANEncoder(PitchMotor);
+  //private final CANEncoder ArmEncoder = SpinMotor.getEncoder(EncoderType.kQuadrature, 8192);
+  private final Encoder ArmEncoder = new Encoder(4, 5, false, EncodingType.k4X);
+  //Starting position reset at 0, fully down is -418.75, fully up in game position is -84.75
+
+  public void ArmUp() {
+
+  }
 
   public PIDArm() {
     super(
@@ -26,12 +39,17 @@ public class PIDArm extends PIDSubsystem {
 
   @Override
   public void useOutput(double output, double setpoint) {
-    // Use the output here
+    PitchMotor.set(output);
   }
 
   @Override
   public double getMeasurement() {
     // Return the process variable measurement here
-    return 0;
+    return ArmEncoder.getDistance();
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("ArmPosition", ArmEncoder.getDistance());
   }
 }
