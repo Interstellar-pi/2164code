36cc51c510f2b61de2526da70246be6aef1ca368
will4623 <60483732+will4623@users.noreply.github.com>
2020-03-10 12:51:16 -0500

    IDK

--- 2019-2020 season code/src/main/java/frc/robot/commands/GoLeft1.java
+++ 2019-2020 season code/src/main/java/frc/robot/commands/GoLeft1.java
@@ -8,18 +8,23 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
 
 public class GoLeft1 extends CommandBase {
   /**
    * Creates a new GoLeft1.
    */
-  public GoLeft1() {
+  DriveTrain subsystem;
+  public GoLeft1(DriveTrain Subsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Subsystem);
+    subsystem = Subsystem;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
